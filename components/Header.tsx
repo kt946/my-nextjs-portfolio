bd00233f4dd7fb5ed2f951fd0b7da8c51f6ea4b1
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-13 19:16:24 -0500

    added mobile menu functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,18 @@ import Link from 'next/link';
 import { HiMenu } from 'react-icons/hi';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { MdOutlineEmail } from 'react-icons/md';
+import { GrClose } from 'react-icons/gr';
 
 type Props = {};
 
 const Header = (props: Props) => {
-  const [toggle, setToggle] = useState(false);
+  const [navMenu, setNavMenu] = useState(false);
+  const [menuButton, setMenuButton] = useState(false);
+
+  const displayMobileMenu = () => {
+    setNavMenu(!navMenu);
+    setMenuButton(!menuButton);
+  };
 
   const navLinks = [
     {
@@ -51,29 +58,24 @@ const Header = (props: Props) => {
   ];
 
   return (
-    <header className="bg-black/40 sticky top-0 z-99 backdrop-blur py-2 px-5 text-base border-b border-gray-500/50">
+    <header className="bg-black/40 fixed w-full top-0 z-20 backdrop-blur py-3 px-3 sm:px-5 text-base border-b border-slate-500/50">
       <nav className="flex justify-between items-center max-w-7xl mx-auto">
         {/* Logo */}
         <Link
           href="/"
-          className="hover:opacity-80 transition duration-300"
+          className="hover:opacity-70 transition duration-300"
         >
           <Image
             src="/assets/portfolio-logo.svg"
             alt="Portfolio Logo"
             width={0}
             height={0}
-            priority
-            className="w-auto h-7"
+            priority={true}
+            className="w-auto max-w-xs h-6"
           />
         </Link>
 
-        <HiMenu
-          className="text-4xl cursor-pointer md:hidden"
-          onClick={() => setToggle(true)}
-        />
-
-        {/* Navigation */}
+        {/* Navigation Links */}
         <ul className="hidden md:flex flex-row items-center space-x-6 tracking-wider">
           {navLinks.map((navLink) => (
             <Link
@@ -86,7 +88,7 @@ const Header = (props: Props) => {
           ))}
           <Link
             href="/"
-            className="py-1.5 px-4 text-sky-500 border hover:text-white border-sky-500 hover:bg-sky-500 rounded-full transition duration-300"
+            className="py-1 px-4 text-sky-500 border hover:text-white border-sky-500 hover:bg-sky-500 rounded-full transition duration-300"
           >
             Resume
           </Link>
@@ -100,6 +102,33 @@ const Header = (props: Props) => {
             </Link>
           ))}
         </ul>
+
+        {/* Mobile Menu Button */}
+        <button
+          type="button"
+          className={`hamburger hamburger--spin p-2 pb-0 z-30 md:hidden ${menuButton && 'is-active'}`}
+          onClick={() => displayMobileMenu()}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+
+        {/* Mobile Menu */}
+        {/* Overlay */}
+        <div
+          className={`${
+            navMenu ? 'md:hidden inset-0 w-full h-screen bg-black/70 backdrop-blur-sm ease-in-out duration-300' : ''
+          } fixed`}
+          onClick={() => displayMobileMenu()}
+        >
+          {/* Menu */}
+          <div
+            className={`${
+              navMenu ? 'right-0' : 'right-[-100%]'
+            } fixed top-0 w-full max-w-xs h-screen p-6 py-2 bg-slate-800 ease-in-out duration-300`}
+          ></div>
+        </div>
       </nav>
     </header>
   );
