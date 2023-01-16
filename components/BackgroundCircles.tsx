eec56901007f18b13f88d695dcf48b9b3cd2f6e9
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-15 19:35:47 -0500

    updated hero background animation and spacing

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -17,15 +17,15 @@ const BackgroundCircles = (props: Props) => {
       transition={{ duration: 1.5 }}
       className="relative flex justify-center items-center"
     >
-      <div className="h-[150px] w-[150px] absolute border-2 border-sky-500/25 rounded-full animate-ping" />
-      <div className="h-[300px] w-[300px] absolute border-dashed border-[12px] border-r-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite_reverse]" />
-      <div className="h-[400px] w-[400px] absolute border-8 border-b-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite]" />
-      <div className="h-[500px] w-[500px] absolute border-dotted border-8 border-l-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite_reverse]" />
-      <div className="h-[600px] w-[600px] absolute border-8 border-t-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite]" />
-      <div className="h-[700px] w-[700px] absolute border-2 border-sky-500/50 rounded-full animate-pulse" />
-      <div className="h-[800px] w-[800px] absolute border-double border-[10px] border-x-transparent border-sky-500/20 rounded-full animate-[spin_10s_linear_infinite_reverse]" />
-      <div className="h-[900px] w-[900px] absolute border-double border-[10px] border-y-transparent border-sky-500/20 rounded-full animate-[spin_10s_linear_infinite]" />
-      <div className="h-[950px] w-[950px] absolute border-[10px] border-sky-500/25 rounded-full" />
+      <div className="h-[75px] md:h-[150px] w-[75px] md:w-[150px] absolute border md:border-2 border-sky-500/25 rounded-full animate-ping" />
+      <div className="h-[150px] md:h-[300px] w-[150px] md:w-[300px] absolute border-dashed border-4 md:border-[12px] border-r-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite_reverse]" />
+      <div className="h-[200px] md:h-[400px] w-[200px] md:w-[400px] absolute border-4 md:border-8 border-b-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite]" />
+      <div className="h-[250px] md:h-[500px] w-[250px] md:w-[500px] absolute border-dotted border-4 md:border-8 border-l-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite_reverse]" />
+      <div className="h-[300px] md:h-[600px] w-[300px] md:w-[600px] absolute border-4 md:border-8 border-t-transparent border-sky-500/10 rounded-full animate-[spin_10s_linear_infinite]" />
+      <div className="h-[350px] md:h-[700px] w-[350px] md:w-[700px] absolute border md:border-2 border-sky-500/50 rounded-full animate-pulse" />
+      <div className="h-[400px] md:h-[800px] w-[400px] md:w-[800px] absolute border-double border-2 md:border-[10px] border-x-transparent border-sky-500/20 rounded-full animate-[spin_10s_linear_infinite_reverse]" />
+      <div className="h-[450px] md:h-[900px] w-[450px] md:w-[900px] absolute border-double border-2 md:border-[10px] border-y-transparent border-sky-500/20 rounded-full animate-[spin_10s_linear_infinite]" />
+      <div className="h-[500px] md:h-[950px] w-[500px] md:w-[950px] absolute border-2 md:border-[10px] border-sky-500/20 rounded-full" />
     </motion.div>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,10 +16,12 @@ const Hero = (props: Props) => {
   return (
     <section
       id="home"
-      className="md:pt-40 w-full min-h-screen flex flex-col items-center justify-center text-center"
+      className="md:pt-40 w-full h-[600px] min-h-screen flex flex-col items-center justify-center text-center"
     >
+      {/* Background Animation */}
       <BackgroundCircles />
 
+      {/* Center Image Container */}
       <motion.div
         initial={{
           opacity: 0,
@@ -32,9 +34,9 @@ const Hero = (props: Props) => {
         className="relative flex justify-center items-center"
       >
         {/* Outer Ring */}
-        <div className="absolute w-[134px] h-[134px] rounded-full bg-gradient-to-b from-sky-600 to-cyan-400 shadow-md shadow-cyan-400/50 animate-[spin_3s_linear_infinite]" />
+        <div className="absolute w-[100px] h-[100px] md:w-[134px] md:h-[134px] rounded-full bg-gradient-to-b from-sky-600 to-cyan-400 shadow-md shadow-cyan-400/50 animate-[spin_3s_linear_infinite]" />
         {/* Inner Circle */}
-        <div className="absolute w-32 h-32 flex flex-col justify-center bg-black rounded-full">
+        <div className="absolute w-24 h-24 md:w-32 md:h-32 flex flex-col justify-center bg-black rounded-full">
           {/* Image */}
           <Image
             src="/assets/portfolio-logo.svg"
@@ -47,12 +49,13 @@ const Hero = (props: Props) => {
         </div>
       </motion.div>
 
-      <div className="z-10 mt-28">
+      {/* Heading */}
+      <div className="z-10 mt-20 md:mt-28">
         <h2 className="pb-2 text-lg md:text-xl lg:text-2xl font-bold tracking-widest">Hi, my name is</h2>
-        <h1 className="pb-2 text-5xl md:text-6xl lg:text-7xl uppercase font-extrabold text-sky-500 tracking-widest">
+        <h1 className="px-6 pb-2 text-5xl md:text-6xl lg:text-7xl uppercase font-extrabold text-sky-500 tracking-widest">
           Kyle Tang
         </h1>
-        <h2 className="uppercase text-2xl md:text-3xl lg:text-4xl font-extrabold px-10 tracking-widest">
+        <h2 className="px-6 h-10 uppercase text-2xl md:text-3xl lg:text-4xl font-extrabold tracking-widest">
           <span>{text}</span>
           <Cursor cursorColor="#0ea5e9" />
         </h2>
