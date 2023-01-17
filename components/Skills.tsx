862f1df4692dcfbed28ab30cb1dd6ad8e1b1b1c8
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-17 18:47:38 -0500

    updated layout and stying of Skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -20,6 +20,10 @@ const Skills = (props: Props) => {
     'Tailwind CSS',
     'NextJS',
     'Framer Motion',
+    'Python',
+    'Lorem-1',
+    'asd',
+    'qwerty',
   ];
 
   // Back-end skills
@@ -29,15 +33,16 @@ const Skills = (props: Props) => {
     'AJAX',
     'Third-party APIs',
     'Node',
-    'Object-Oriented Programming (OOP)',
+    'OOP',
     'Express',
     'REST API',
     'SQL, MySQL, Sequelize',
-    'Object-Relational Mapping (ORM)',
-    'Model-View-Controller (MVC)',
+    'ORM',
+    'MVC',
     'Handlebars',
     'NoSQL, MongoDB, Mongoose',
     'MERN',
+    'Sanity',
   ];
 
   return (
@@ -46,50 +51,61 @@ const Skills = (props: Props) => {
       className="mb-20 px-6 py-12 scroll-mt-16 border border-amber-500"
     >
       {/* Content Container */}
-      <div className="mx-auto w-full max-w-[1280px] flex flex-col border border-emerald-500">
+      <div className="mx-auto w-full max-w-[1280px] flex flex-col">
         {/* Section Title */}
         <div className="mb-12 md:mb-24">
           <h2 className="mb-3 text-3xl md:text-4xl text-center md:text-left text-sky-500 uppercase tracking-widest font-semibold">
             Skills
           </h2>
           <h3 className="text-3xl sm:text-5xl md:text-6xl text-center md:text-left font-bold">
-            Creating digital solutions with style
+            Crafting digital solutions with style
           </h3>
         </div>
 
         {/* Skills Container */}
-        <div className="relative w-full max-w-full flex flex-col md:flex-row justify-center">
-          {/* Skill Card */}
-          <div className="w-full max-w-7xl flex flex-col md:flex-row rounded-xl shadow-lg bg-slate-800 text-center">
+        <div className="w-full max-w-full flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-8">
+          {/* Frontend Skill Card */}
+          <div className="w-full max-w-7xl p-3 flex flex-col rounded-xl shadow-lg bg-slate-800 text-center">
+            {/* Card Header */}
+            <div className="mb-6 flex flex-col items-center">
+              {/* Frontend Icon */}
+              <div className="mb-4 p-4 rounded-full bg-slate-900 text-3xl text-sky-500">
+                <HiCode />
+              </div>
+              {/* Frontend Title */}
+              <h3 className="w-min text-xl sm:2xl md:text-3xl font-semibold uppercase tracking-widest">
+                Frontend Development
+              </h3>
+            </div>
+            {/* Card Body */}
             {/* Frontend List */}
-            <div className="w-full px-3 border-b md:border-b-0 md:border-r border-sky-500/50">
-              <div className="my-6 flex flex-col items-center">
-                <div className="mb-4 p-4 rounded-full bg-slate-900 text-3xl text-sky-500">
-                  <HiCode />
-                </div>
-                <h3 className="text-xl md:text-2xl font-semibold uppercase tracking-widest">Frontend Skills</h3>
-              </div>
-              <ul className="mb-6 space-y-2 text-lg text-slate-400 font-medium">
-                {frontEndSkills.map((skill) => (
-                  <li key={skill}>{skill}</li>
-                ))}
-              </ul>
-            </div>
+            <ul className="h-full py-4 space-y-2 bg-slate-900 rounded-xl text-lg text-slate-300">
+              {frontEndSkills.map((skill) => (
+                <li key={skill}>{skill}</li>
+              ))}
+            </ul>
+          </div>
 
-            {/* Backend List */}
-            <div className="w-full px-3">
-              <div className="my-6 flex flex-col items-center">
-                <div className="mb-4 p-4 rounded-full bg-slate-900 text-3xl text-sky-500">
-                  <HiDatabase />
-                </div>
-                <h3 className="text-xl md:text-2xl font-semibold uppercase tracking-widest">Backend Skills</h3>
+          {/* Backend Skill Card */}
+          <div className="w-full max-w-7xl p-3 flex flex-col rounded-xl shadow-lg bg-slate-800 text-center">
+            {/* Card Header */}
+            <div className="mb-6 flex flex-col items-center">
+              {/* Backend Icon */}
+              <div className="mb-4 p-4 rounded-full bg-slate-900 text-3xl text-sky-500">
+                <HiDatabase />
               </div>
-              <ul className="mb-6 space-y-2 text-lg text-slate-400 font-medium">
-                {backEndSkills.map((skill) => (
-                  <li key={skill}>{skill}</li>
-                ))}
-              </ul>
+              {/* Backend Title */}
+              <h3 className="w-min text-xl sm:2xl md:text-3xl font-semibold uppercase tracking-widest">
+                Backend Development
+              </h3>
             </div>
+            {/* Card Body */}
+            {/* Backend List */}
+            <ul className="h-full py-4 space-y-2 bg-slate-900 rounded-xl text-lg text-slate-300">
+              {backEndSkills.map((skill) => (
+                <li key={skill}>{skill}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
