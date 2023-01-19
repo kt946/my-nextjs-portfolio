aa4d9a1416b81977712d4f84aa8c4dfb78182490
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-18 22:58:16 -0500

    added small edits to contact card and footer

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -41,11 +41,11 @@ const Contact = (props: Props) => {
                 <HiOutlineChatBubbleBottomCenterText />
               </span>
             </h4>
-            <p className="text-slate-400 text-lg md:px-2">
-              I&apos;m currently looking for full-time positions, but if you got an exciting opportunity to share or if
+            <p className="text-slate-300 text-lg md:px-2">
+              I&apos;m currently looking for full-time positions, but if you got an exciting opportunity you need help with or if
               you just want to say hi, my inbox is always open!
             </p>
-            <p className="text-slate-400 text-lg md:px-2">
+            <p className="text-slate-300 text-lg md:px-2">
               Send me a message through the form or connect with me on socials!
             </p>
             {/* Social Links */}
@@ -54,7 +54,7 @@ const Contact = (props: Props) => {
                 <Link
                   key={socialLink.name}
                   href={socialLink.url}
-                  className="p-2 rounded-full text-2xl text-slate-400 hover:text-sky-500 border-2 border-slate-500 hover:border-sky-500 custom-transition"
+                  className="p-2 md:p-3 rounded-full text-2xl md:text-3xl text-slate-400 hover:text-sky-500 border-2 md:border-[3px] border-slate-500 hover:border-sky-500 custom-transition"
                 >
                   {socialLink.icon}
                 </Link>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,15 @@ import { SiTailwindcss, SiNextdotjs, SiFramer } from 'react-icons/si';
 type Props = {};
 
 const Footer = (props: Props) => {
+  const year = new Date().getFullYear();
+
   return (
     <section
       id="footer"
       className="px-4"
     >
       <div className="py-12 border-t border-slate-500/50 flex flex-col items-center font-medium text-center">
-        <p className="text-lg mb-4">Designed and built by Me &copy; 2023</p>
+        <p className="text-lg mb-4">&copy; {year} Kyle Tang</p>
         <p className="flex items-center">
           Made with
           <span className="pl-2 flex text-3xl space-x-2">
