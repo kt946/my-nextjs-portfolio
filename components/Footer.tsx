e6bdcf006824eefe18f4ec359dd87b47786c697c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-01-18 20:43:09 -0500

    added icons to footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { SiTailwindcss, SiNextdotjs, SiFramer } from 'react-icons/si';
 
 type Props = {};
 
 const Footer = (props: Props) => {
-
   return (
     <section
       id="footer"
@@ -11,7 +11,14 @@ const Footer = (props: Props) => {
     >
       <div className="py-12 border-t border-slate-500/50 flex flex-col items-center font-medium text-center">
         <p className="text-lg mb-4">Designed and built by Me &copy; 2023</p>
-        <p>Made with NextJS, Tailwind, and Framer</p>
+        <p className="flex items-center">
+          Made with
+          <span className="pl-2 flex text-3xl space-x-2">
+            <SiTailwindcss />
+            <SiNextdotjs />
+            <SiFramer />
+          </span>
+        </p>
       </div>
     </section>
   );
